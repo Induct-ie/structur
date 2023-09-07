5000cb1a7a7165e4f66b1afe53f99a32a1c89a4f
Samir Bioud <samir@kronsy.dev>
2023-09-07 19:13:07 +0100

    Removed target dir from VC

--- target/debug/deps/structur-df1b260954bd3473.d
+++
@@ -1,5 +0,0 @@
-/home/kronsy/induct/opensource/structur/target/debug/deps/structur-df1b260954bd3473.rmeta: src/lib.rs
-
-/home/kronsy/induct/opensource/structur/target/debug/deps/structur-df1b260954bd3473.d: src/lib.rs
-
-src/lib.rs:
